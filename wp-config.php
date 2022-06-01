d43cdd0e4d296d5a700cc814902bc60fc13365a1
Sean Coomer Wassp <105342613+SeanWassp@users.noreply.github.com>
2022-06-01 13:18:21 +0200

    fix theme

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'forge' );
+define( 'DB_NAME', 'forge' ); 
 
 /** Database username */
 define( 'DB_USER', 'testsite' ); 
