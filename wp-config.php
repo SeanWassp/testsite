b68ea82f89790f8cca0cefcc23160938223893aa
Sean Coomer Wassp <105342613+SeanWassp@users.noreply.github.com>
2022-06-01 12:25:48 +0200

    changes

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'forge' );
+define( 'DB_NAME', 'wasspDB' );
 
 /** Database username */
-define( 'DB_USER', 'testsite' ); 
+define( 'DB_USER', 'Sean@Wassp' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'testsite@2022' );
+define( 'DB_PASSWORD', 'Scoomes@3576' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
